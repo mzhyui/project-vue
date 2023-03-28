2b3240155c3a5dd59547db40762dbf3ff10bcd01
mzhyui <mzhyui@hotmail.com>
2023-03-28 16:39:14 +0800

    added base framework

---
+++ src/components/background_manage.vue
@@ -0,0 +1,131 @@
+<template>  
+  <el-container style="height: 500px; border: 1px solid #eee">
+    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+      <el-menu :default-openeds="['1', '3']">
+        <el-submenu index="1">
+          <template slot="title"><i class="el-icon-message"></i>社区A</template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="1-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-submenu index="2">
+          <template slot="title"><i class="el-icon-menu"></i>社区B</template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="2-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="2-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-submenu index="3">
+          <template slot="title"><i class="el-icon-setting"></i>社区C</template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="3-1">选项1</el-menu-item>
+            <el-menu-item index="3-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="3-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="3-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
+    
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px">
+        <el-dropdown>
+          <i class="el-icon-setting" style="margin-right: 15px"></i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>查看</el-dropdown-item>
+            <el-dropdown-item>新增</el-dropdown-item>
+            <el-dropdown-item>删除</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+        <span>王小虎</span>
+      </el-header>
+      
+      <el-main>
+        <el-table :data="tableData">
+          <el-table-column prop="id" label="档 案 编 号" width="140">
+          </el-table-column>
+          <el-table-column prop="issue" label="病种" width="140">
+          </el-table-column>
+          <el-table-column prop="name" label="姓名" width="120">
+          </el-table-column>
+          <el-table-column prop="age" label="年龄">
+          </el-table-column>
+          <el-table-column prop="IdCard" label="身份证" width="150">
+          </el-table-column>
+          <el-table-column prop="phone" label="手机号" width="150">
+          </el-table-column>
+          <el-table-column prop="Status" label="服务状态" width="120">
+          </el-table-column>
+          <el-table-column prop="FromDate" label="服务开始日期" width="130">
+          </el-table-column>
+          <el-table-column prop="ToDate" label="服务结束日期" width="130">
+          </el-table-column>
+        </el-table>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+  
+<style>
+.el-header {
+  background-color: #B3C0D1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+</style>
+  
+<script>
+export default {
+  name: 'bgM'
+  ,data() {
+    const item1 = {
+      id: '4145',
+      issue: 'A',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
+      , age: '41'
+      , IdCard: '320582xx'
+      , phone: '138xxx'
+      , Status: '已签约'
+      , FromDate: '2021'
+      , ToDate: '2022'
+    };
+    // const item2 = {
+    //   date: '4146',
+    //   issue: 'B',
+    //   name: '王小虎',
+    //   address: '上海市普陀区金沙江路 1518 弄'
+    // };
+    return {
+      tableData: Array(5).fill(item1)
+    }
+  }
+};
+</script>
